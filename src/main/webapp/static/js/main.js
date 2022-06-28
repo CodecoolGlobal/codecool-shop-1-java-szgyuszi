02c6ad90f404fa6156adf48cc20ed00802f86737
szgyuszi <szgybence@gmail.com>
2022-06-28 14:09:24 +0200

    feat: add href to card in sliders

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,1 +1,16 @@
-console.log("helo")
+init();
+
+function init() {
+    addEventToCards()
+}
+
+
+function addEventToCards() {
+    const cards = document.querySelectorAll(".media-element");
+    for (let card of cards) {
+        card.addEventListener("click", (event)=> {
+            const id = event.target.dataset.id
+            window.location.href = `/movie?id=${id}`;
+        })
+    }
+}
