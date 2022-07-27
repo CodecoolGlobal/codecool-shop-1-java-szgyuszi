3997d33184b5b2c079ec272375e9817a1b1ed4d1
kristof553 <kristofmolnar553@gmail.com>
2022-07-27 11:18:16 +0200

    feat: implement registerDao and RegisterDaoMem

---
+++ src/main/java/com/codecool/shop/dao/RegisterDao.java
@@ -0,0 +1,11 @@
+package com.codecool.shop.dao;
+
+public interface RegisterDao {
+
+    void registerUser();
+
+    void deleterUser();
+
+    void updateUser();
+
+}
---
+++ src/main/java/com/codecool/shop/dao/implementation/RegisterDaoMem.java
@@ -0,0 +1,6 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.RegisterDao;
+
+public class RegisterDaoMem  {
+}
